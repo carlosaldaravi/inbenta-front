3769d95dd362fb5edcbc89722b930edc110ce5db
Carlos Aldaravi <carlosaldaravi11@gmail.com>
2021-04-16 12:35:10 +0100

    backend url updated

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ export const config = {
   // },
   // apiURL: "http://localhost/inbenta/api.php",
   graphqlURL: "https://inbenta-graphql-swapi-prod.herokuapp.com/api",
-  apiURL: "https://inbenta-api.herokuapp.com/",
+  apiURL: "https://inbenta-api.herokuapp.com/api/api.php",
   network: {
     SUCCESS: 200,
     UNAUTHORIZED: 401,
